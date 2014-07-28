ed221cdfbf451d739c1609a71d4720d3751f2bcc
Serg Baburin <serg.baburin@gmail.com>
2014-07-29 00:17:38 +0400

    Added LinkedIn oauth provider.

---
+++ main/apps/auth/providers/linkedin/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+CONFIG = {
+    'name': 'linkedin',
+    'key_fields': {
+        'linkedin_api_key': 'API Key',
+        'linkedin_secret_key': 'Secret Key',
+      },
+    'template_admin': 'auth/providers/linkedin/admin_config.html',
+    'template_signin': 'auth/providers/linkedin/signin.html',
+  }
---
+++ main/apps/auth/providers/linkedin/views.py
@@ -0,0 +1,103 @@
+# coding: utf-8
+from google.appengine.api import urlfetch
+from flask.ext import oauth
+import flask
+import unidecode
+
+from apps.auth import helpers
+from apps.auth.models import AuthProviders
+from apps.user import models
+from core import util
+from .import CONFIG
+
+PROVIDERS_DB = AuthProviders.get_master_db()
+PROVIDER_NAME = CONFIG['name']
+
+bp = flask.Blueprint(
+    'auth.%s' % PROVIDER_NAME,
+    __name__,
+    url_prefix='/auth',
+    template_folder='templates',
+  )
+
+bps = flask.Blueprint(
+    'auth.%s.service' % PROVIDER_NAME,
+    __name__,
+    url_prefix='/_s/callback/%s' % PROVIDER_NAME,
+  )
+
+provider_oauth = oauth.OAuth()
+
+provider = provider_oauth.remote_app(
+    'linkedin',
+    base_url='https://api.linkedin.com/v1/',
+    request_token_url=None,
+    access_token_url='https://www.linkedin.com/uas/oauth2/accessToken',
+    access_token_params={'grant_type': 'authorization_code'},
+    access_token_method='POST',
+    authorize_url='https://www.linkedin.com/uas/oauth2/authorization',
+    consumer_key=PROVIDERS_DB.get_field('%s_api_key' % PROVIDER_NAME),
+    consumer_secret=PROVIDERS_DB.get_field('%s_secret_key' % PROVIDER_NAME),
+    request_token_params={
+        'scope': 'r_basicprofile r_emailaddress',
+        'state': util.uuid(),
+      },
+  )
+
+
+@bps.route('/oauth-authorized/')
+@provider.authorized_handler
+def authorized(resp):
+  if resp is None:
+    return 'Access denied: error=%s error_description=%s' % (
+        flask.request.args['error'],
+        flask.request.args['error_description'],
+      )
+  flask.session['access_token'] = (resp['access_token'], '')
+  fields = 'id,first-name,last-name,email-address'
+  profile_url = '%speople/~:(%s)?oauth2_access_token=%s' % (
+      provider.base_url, fields, resp['access_token'],
+    )
+  result = urlfetch.fetch(
+      profile_url,
+      headers={'x-li-format': 'json', 'Content-Type': 'application/json'}
+    )
+  try:
+    content = flask.json.loads(result.content)
+  except ValueError:
+    return "Unknown error: invalid response from LinkedIn"
+  if result.status_code != 200:
+    return 'Unknown error: status=%s message=%s' % (
+        content['status'], content['message'],
+      )
+  user_db = retrieve_user_from_linkedin(content)
+  return helpers.signin_user_db(user_db)
+
+
+@provider.tokengetter
+def get_linkedin_oauth_token():
+  return flask.session.get('access_token')
+
+
+@bp.route('/signin/%s/' % PROVIDER_NAME)
+def signin():
+  flask.session['access_token'] = None
+  helpers.save_request_params()
+  return provider.authorize(callback=flask.url_for(
+      'linkedin_authorized', _external=True
+    ))
+
+
+def retrieve_user_from_linkedin(response):
+  auth_id = '%s_%s' % (PROVIDER_NAME, response['id'])
+  user_db = models.User.get_by('auth_ids', auth_id)
+  if user_db:
+    return user_db
+  full_name = ' '.join([response['firstName'], response['lastName']]).strip()
+  return helpers.create_user_db(
+      auth_id,
+      full_name,
+      response['emailAddress'] or unidecode.unidecode(full_name),
+      response['emailAddress'],
+    )
+
