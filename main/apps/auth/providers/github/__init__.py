2532e8004bf3d098268d4d04a4caae41975b34e8
Serg Baburin <serg.baburin@gmail.com>
2014-07-28 18:18:41 +0400

    Added GitHub oauth provider.

---
+++ main/apps/auth/providers/github/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+CONFIG = {
+    'name': 'github',
+    'key_fields': {
+      'github_client_id': 'App ID',
+      'github_client_secret': 'App Secret',
+      },
+    'template_admin': 'auth/providers/github/admin_config.html',
+    'template_signin': 'auth/providers/github/signin.html',
+  }
+
---
+++ main/apps/auth/providers/github/views.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+from flask.ext import oauth
+import flask
+
+from apps.auth import helpers
+from apps.auth.models import AuthProviders
+from apps.user import models
+from .import CONFIG
+
+PROVIDERS_DB = AuthProviders.get_master_db()
+PROVIDER_NAME = CONFIG['name']
+
+bp = flask.Blueprint(
+    'auth.%s' % PROVIDER_NAME,
+    __name__,
+    url_prefix='/auth',
+    template_folder='templates',
+  )
+
+bps = flask.Blueprint(
+    'auth.%s.service' % PROVIDER_NAME,
+    __name__,
+    url_prefix='/_s/callback/%s' % PROVIDER_NAME,
+  )
+
+provider_oauth = oauth.OAuth()
+
+provider = provider_oauth.remote_app(
+    PROVIDER_NAME,
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+    consumer_key=PROVIDERS_DB.get_field('%s_client_id' % PROVIDER_NAME),
+    consumer_secret=PROVIDERS_DB.get_field('%s_client_secret' % PROVIDER_NAME),
+    request_token_params={'scope': 'user:email'},
+  )
+
+
+@bps.route('/oauth-authorized/')
+@provider.authorized_handler
+def authorized(resp):
+  if resp is None:
+    return 'Access denied: error=%s' % flask.request.args['error']
+  flask.session['oauth_token'] = (resp['access_token'], '')
+  me = provider.get('user')
+  user_db = retrieve_user_from_github(me.data)
+  return helpers.signin_user_db(user_db)
+
+
+@provider.tokengetter
+def get_github_oauth_token():
+  return flask.session.get('oauth_token')
+
+
+@bp.route('/signin/%s/' % PROVIDER_NAME)
+def signin():
+  helpers.save_request_params()
+  return provider.authorize(callback=flask.url_for(
+      'auth.%s.service.authorized' % PROVIDER_NAME, _external=True
+    ))
+
+
+def retrieve_user_from_github(response):
+  auth_id = '%s_%s' % (PROVIDER_NAME, str(response['id']))
+  user_db = models.User.get_by('auth_ids', auth_id)
+  if user_db:
+    return user_db
+  return helpers.create_user_db(
+      auth_id,
+      response['name'] or response['login'],
+      response['login'],
+      response['email'] or '',
+    )
