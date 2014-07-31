1b36d2d5b4398af6ba5bf1b24ef3fb084c0ccfcc
Serg Baburin <serg.baburin@gmail.com>
2014-08-01 00:44:39 +0400

    Merged with gae-init master.

--- main/apps/auth/providers/facebook/views.py
+++ main/apps/auth/providers/facebook/views.py
@@ -72,6 +72,6 @@ def retrieve_user_from_facebook(response):
   return helpers.create_user_db(
       auth_id,
       response['name'],
-      response['username'] if 'username' in response else response['id'],
-      response['email'],
+      response.get('username', response['id']),
+      response.get('email', ''),
     )
