6476742d6bbacf5a45d04757e3e1a6cf6963d3f9
Serg Baburin <serg.baburin@gmail.com>
2014-08-02 01:23:36 +0400

    Added Microsoft oauth provider.

---
+++ main/apps/auth/providers/microsoft/views.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+from flask.ext import oauth
+import flask
+
+from apps.auth import helpers
+from apps.auth.models import AuthProviders
+from apps.user import models
+from .import CONFIG
+
+
+PROVIDERS_DB = AuthProviders.get_master_db()
+PROVIDER_NAME = CONFIG['name']
+
+bp = flask.Blueprint(
+    'auth.%s' % PROVIDER_NAME,
+    __name__,
+    url_prefix='/auth',
+    template_folder='templates',
+  )
+
+bps = flask.Blueprint(
+    'auth.%s.service' % PROVIDER_NAME,
+    __name__,
+    url_prefix='/_s/callback/%s' % PROVIDER_NAME,
+  )
+
+provider_oauth = oauth.OAuth()
+
+provider = provider_oauth.remote_app(
+    PROVIDER_NAME,
+    base_url='https://apis.live.net/v5.0/',
+    request_token_url=None,
+    access_token_url='https://login.live.com/oauth20_token.srf',
+    access_token_method='POST',
+    access_token_params={'grant_type': 'authorization_code'},
+    authorize_url='https://login.live.com/oauth20_authorize.srf',
+    consumer_key=PROVIDERS_DB.get_field('%s_client_id' % PROVIDER_NAME),
+    consumer_secret=PROVIDERS_DB.get_field('%s_client_secret' % PROVIDER_NAME),
+    request_token_params={'scope': 'wl.emails'},
+  )
+
+
+@bps.route('/oauth-authorized/')
+@provider.authorized_handler
+def authorized(resp):
+  if resp is None:
+    return 'Access denied: error=%s error_description=%s' % (
+        flask.request.args['error'],
+        flask.request.args['error_description'],
+      )
+  flask.session['oauth_token'] = (resp['access_token'], '')
+  me = provider.get(
+      'me',
+      data={'access_token': resp['access_token']},
+      headers={'accept-encoding': 'identity'},
+    ).data
+  if me.get('error'):
+    return 'Unknown error: error:%s error_description:%s' % (
+        me['code'],
+        me['message'],
+      )
+  user_db = retrieve_user_from_microsoft(me)
+  return helpers.signin_user_db(user_db)
+
+
+@provider.tokengetter
+def get_microsoft_oauth_token():
+  return flask.session.get('oauth_token')
+
+
+@bp.route('/signin/%s/' % PROVIDER_NAME)
+def signin():
+  helpers.save_request_params()
+  return provider.authorize(callback=flask.url_for(
+      'auth.microsoft.service.authorized', _external=True
+    ))
+
+
+def retrieve_user_from_microsoft(response):
+  auth_id = '%s_%s' % (PROVIDER_NAME, response['id'])
+  user_db = models.User.get_by('auth_ids', auth_id)
+  if user_db:
+    return user_db
+  email = response['emails']['preferred'] or response['emails']['account']
+  return helpers.create_user_db(
+      auth_id,
+      response['name'] or '',
+      email,
+      email=email,
+    )
