99d2aa6338ed2fa0d46c66b752d33a010c74f2b8
Serg Baburin <serg.baburin@gmail.com>
2014-09-06 20:19:36 +0400

    Merged with gae-init/gae-init: Flask security key is part of security keys now

--- main/apps/admin/forms.py
+++ main/apps/admin/forms.py
@@ -7,14 +7,39 @@ import models
 
 
 class ConfigUpdateForm(wtf.Form):
-  analytics_id = wtforms.StringField('Tracking ID', filters=[util.strip_filter])
-  announcement_html = wtforms.TextAreaField('Announcement HTML', filters=[util.strip_filter])
-  announcement_type = wtforms.SelectField('Announcement Type', choices=[(t, t.title()) for t in models.Config.announcement_type._choices])
-  brand_name = wtforms.StringField('Brand Name', [wtforms.validators.required()], filters=[util.strip_filter])
-  check_unique_email = wtforms.BooleanField('Check for the uniqueness of the verified emails')
-  feedback_email = wtforms.StringField('Feedback Email', [wtforms.validators.optional(), wtforms.validators.email()], filters=[util.email_filter])
-  flask_secret_key = wtforms.StringField('Secret Key', [wtforms.validators.optional()], filters=[util.strip_filter])
-  notify_on_new_user = wtforms.BooleanField('Send an email notification when a user signs up')
+  analytics_id = wtforms.StringField(
+      'Tracking ID', filters=[util.strip_filter]
+    )
+  announcement_html = wtforms.TextAreaField(
+      'Announcement HTML', filters=[util.strip_filter]
+    )
+  announcement_type = wtforms.SelectField(
+      'Announcement Type',
+      choices=[(t, t.title()) for t in models.Config.announcement_type._choices]
+    )
+  brand_name = wtforms.StringField(
+      'Brand Name',
+      [wtforms.validators.required()],
+      filters=[util.strip_filter]
+    )
+  check_unique_email = wtforms.BooleanField(
+      'Check for the uniqueness of the verified emails'
+    )
+  feedback_email = wtforms.StringField(
+      'Feedback Email',
+      [wtforms.validators.optional(), wtforms.validators.email()],
+      filters=[util.email_filter]
+    )
+  flask_secret_key = wtforms.StringField(
+      'Flask Secret Key',
+      [wtforms.validators.optional()],
+      filters=[util.strip_filter]
+    )
+  notify_on_new_user = wtforms.BooleanField(
+      'Send an email notification when a user signs up'
+    )
   send_error_reports = wtforms.BooleanField('Send daily error reports')
   verify_email = wtforms.BooleanField('Verify user emails')
-  yandex_metrika_counter_number = wtforms.StringField('Counter number', filters=[util.strip_filter])
+  yandex_metrika_counter_number = wtforms.StringField(
+      'Counter number', filters=[util.strip_filter]
+    )
