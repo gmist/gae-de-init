36ca146ab80591b16ef8f36a041af4646cf78f3d
Serg Baburin <serg.baburin@gmail.com>
2014-09-16 21:31:52 +0400

    Fix copy/past error.

--- main/apps/auth/providers/twitter/__init__.py
+++ main/apps/auth/providers/twitter/__init__.py
@@ -5,17 +5,15 @@ from apps.auth import helpers
 NAME = 'twitter'
 
 FIELDS = {
-    helpers.get_consumer_key_field_name(NAME): 'Consumer Key',
-    helpers.get_consumer_secret_field_name(NAME): 'Consumer Secret',
+    helpers.get_consumer_key_field_name(NAME): 'API key',
+    helpers.get_consumer_secret_field_name(NAME): 'API secret',
   }
 
 OAUTH = {
-    'base_url': 'https://api.stackexchange.com/2.1/',
-    'request_token_url': None,
-    'access_token_url': 'https://stackexchange.com/oauth/access_token',
-    'access_token_method': 'POST',
-    'authorize_url': 'https://stackexchange.com/oauth',
-    'request_token_params': {},
+    'base_url': 'https://api.twitter.com/1.1/',
+    'request_token_url': 'https://api.twitter.com/oauth/request_token',
+    'access_token_url': 'https://api.twitter.com/oauth/access_token',
+    'authorize_url': 'https://api.twitter.com/oauth/authorize',
   }
 
 CONFIG = helpers.make_provider_config(NAME, FIELDS, OAUTH)
