66015162e4c32c59f032e086459bd4f6361cc09a
Serg Baburin <serg.baburin@gmail.com>
2014-10-23 16:57:10 +0400

    Using YQL for Yahoo OAuth.

--- main/apps/auth/providers/yahoo/__init__.py
+++ main/apps/auth/providers/yahoo/__init__.py
@@ -10,7 +10,7 @@ FIELDS = {
   }
 
 OAUTH = {
-    'base_url': 'https://social.yahooapis.com/',
+    'base_url': 'https://query.yahooapis.com/',
     'request_token_url': 'https://api.login.yahoo.com/oauth/v2/get_request_token',
     'access_token_url': 'https://api.login.yahoo.com/oauth/v2/get_token',
     'authorize_url': 'https://api.login.yahoo.com/oauth/v2/request_auth',
--- main/apps/auth/providers/yahoo/views.py
+++ main/apps/auth/providers/yahoo/views.py
@@ -22,24 +22,16 @@ def authorized():
       resp['oauth_token'],
       resp['oauth_token_secret'],
     )
-
-  try:
-    yahoo_guid = provider.get(
-        '/v1/me/guid',
-        data={'format': 'json', 'realm': 'yahooapis.com'}
-      ).data['guid']['value']
-
-    profile = provider.get(
-        '/v1/user/%s/profile' % yahoo_guid,
-        data={'format': 'json', 'realm': 'yahooapis.com'}
-      ).data['profile']
-  except:
-    flask.flash(
-        u'Something went wrong with Yahoo! sign in. Please try again.',
-        category='danger',
-      )
-    return flask.redirect(util.get_next_url())
-  user_db = retrieve_user_from_yahoo(profile)
+  fields = 'guid, emails, familyName, givenName, nickname'
+  me = provider.get(
+      '/v1/yql',
+      data={
+          'format': 'json',
+          'q': 'select %s from social.profile where guid = me;' % fields,
+          'realm': 'yahooapis.com',
+        },
+    )
+  user_db = retrieve_user_from_yahoo(me.data['query']['results']['profile'])
   return helpers.signin_user_db(user_db)
 
 
@@ -65,10 +57,13 @@ def retrieve_user_from_yahoo(response):
   user_db = models.User.get_by('auth_ids', auth_id)
   if user_db:
     return user_db
-  emails = [e for e in response.get('emails', []) if e.get('handle')]
+  names = [response.get('givenName', ''), response.get('familyName', '')]
+  emails = response.get('emails', {})
+  if not isinstance(emails, list):
+    emails = [emails]
+  emails = [e for e in emails if 'handle' in e]
   emails.sort(key=lambda e: e.get('primary', False))
   email = emails[0]['handle'] if emails else ''
-  names = [response.get('givenName', ''), response.get('familyName', '')]
   return helpers.create_user_db(
       auth_id=auth_id,
       name=' '.join(names).strip() or response['nickname'],
